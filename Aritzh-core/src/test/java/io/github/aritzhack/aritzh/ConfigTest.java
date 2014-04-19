e880264c2ab614d37d392ca41727c646eed101be
Aritz Lopez <aritzhack@gmail.com>
2014-04-19 12:29:28 +0200

    + Added unit tests to test whether the newly added Path config works * Moved deleting the files to the AfterClass method, so that it is done at the end * Changed IOUtil.delete() to Files.delete(), since it will just delete a file, and not a whole folder structure

--- Aritzh-core/src/test/java/io/github/aritzhack/aritzh/ConfigTest.java
+++ Aritzh-core/src/test/java/io/github/aritzhack/aritzh/ConfigTest.java
@@ -16,12 +16,15 @@
 
 package io.github.aritzhack.aritzh;
 
-import io.github.aritzhack.aritzh.util.IOUtil;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
@@ -34,6 +37,9 @@ public class ConfigTest {
     private static final String[] invalidProperties = new String[]{"k ey = val", "key = ", "key =", "key =         ", "     =         ", " = ", "="};
     private static final String[] validCategories = new String[]{" [cat]", "[cat] ", " [ cat]", " [cat ]", "[ cat] ", "[cat ] ", " [cat] ", " [ cat] ", " [cat ] ", " [ cat ] ", "   [  cat  ]   ", "[ca t]", "   [ca   t]  ", "   [   ca t   ]   ", "   [   ca   t   ]   "};
     private static final String[] invalidCategories = new String[]{"[]", "[ ]", "[    ]", "    []    ", "    [   ]   "};
+
+    private static final Path configPath = Paths.get("testPathConfig.cfg");
+    private static final File configFile = new File("testFileConfig.cfg");
 
     @Test
     public void regexTest() {
@@ -63,8 +69,7 @@ public class ConfigTest {
     }
 
     @Test
-    public void keyPropertyTest() throws IOException {
-        File configFile = new File("testConfig.cfg");
+    public void fileReadWriteTest() throws IOException {
         Configuration c = Configuration.newConfig(configFile);
 
         String cat1 = "Category1";
@@ -105,7 +110,55 @@ public class ConfigTest {
         Assert.assertEquals(value21, afterValue21);
         Assert.assertEquals(value22, afterValue22);
         Assert.assertEquals(value23.trim(), afterValue23);
+    }
 
-        IOUtil.delete(configFile);
+    @Test
+    public void pathReadWriteTest() throws IOException {
+        Configuration c = Configuration.newConfig(configPath);
+
+        String cat1 = "Category1";
+
+        String key11 = "Key11", value11 = "Val11",
+                key12 = "Key 12", value12 = "Val 12",
+                key13 = "   Key13   ", value13 = "  Val13  ";
+
+        String cat2 = "Category 2";
+
+        String key21 = "Key21", value21 = "Val21",
+                key22 = "Key 22", value22 = "Val 22",
+                key23 = "   Key23   ", value23 = "  Val23  ";
+
+        c.setProperty(cat1, key11, value11);
+        c.setProperty(cat1, key12, value12);
+        c.setProperty(cat1, key13, value13);
+
+        c.setProperty(cat2, key21, value21);
+        c.setProperty(cat2, key22, value22);
+        c.setProperty(cat2, key23, value23);
+
+        c.save();
+
+        c = Configuration.loadConfig(configPath);
+
+        String afterValue11 = c.getProperty(cat1, key11),
+                afterValue12 = c.getProperty(cat1, key12),
+                afterValue13 = c.getProperty(cat1, key13),
+                afterValue21 = c.getProperty(cat2, key21),
+                afterValue22 = c.getProperty(cat2, key22),
+                afterValue23 = c.getProperty(cat2, key23);
+
+
+        Assert.assertEquals(value11, afterValue11);
+        Assert.assertEquals(value12, afterValue12);
+        Assert.assertEquals(value13.trim(), afterValue13);
+        Assert.assertEquals(value21, afterValue21);
+        Assert.assertEquals(value22, afterValue22);
+        Assert.assertEquals(value23.trim(), afterValue23);
+    }
+
+    @AfterClass
+    public static void after() throws IOException {
+        Files.deleteIfExists(configFile.toPath());
+        Files.deleteIfExists(configPath);
     }
 }
