6594e1729125357084acb71dd49826f9393264fd
Aritz Lopez <aritzhack@gmail.com>
2015-01-04 19:09:58 +0100

    + Added utility methods to get OSLogger for Files + Added a Shutdown hook to close the still-open threads

--- aritzh-core/src/main/java/io/github/aritzhack/aritzh/logging/OSLogger.java
+++ aritzh-core/src/main/java/io/github/aritzhack/aritzh/logging/OSLogger.java
@@ -1,12 +1,17 @@
 package io.github.aritzhack.aritzh.logging;
 
+import com.google.common.collect.Sets;
 import io.github.aritzhack.aritzh.logging.core.ALogger;
 import io.github.aritzhack.aritzh.logging.core.ILogger;
 import io.github.aritzhack.aritzh.logging.core.LogLevel;
 import io.github.aritzhack.aritzh.util.Nullable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Set;
 
 /**
  * @author Aritz Lopez
@@ -14,11 +19,28 @@ import java.io.PrintStream;
 public class OSLogger extends ALogger {
 
     private static final String DEFAULT_FORMAT = "%d - %L %m?t{\n%t}";
+    private static final Set<OSLogger> OS_LOGGERS = Sets.newHashSet();
+
+    static {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                for (OSLogger l : OS_LOGGERS) {
+                    if (!l.isClosed()) {
+                        l.close();
+                    }
+                }
+            }
+        };
+
+        Runtime.getRuntime().addShutdownHook(new Thread(r, "Logger-closing thread"));
+    }
 
     private static ILogger OUT_LOGGER;
     private static ILogger ERR_LOGGER;
-    final PrintStream ps;
-    final String format;
+    private final PrintStream ps;
+    private final String format;
+    private boolean closed = false;
 
     public OSLogger(OutputStream os) {
         this(os, null);
@@ -27,6 +49,31 @@ public class OSLogger extends ALogger {
     public OSLogger(OutputStream os, @Nullable String format) {
         this.ps = new PrintStream(os, true);
         this.format = format == null ? DEFAULT_FORMAT : format;
+        OS_LOGGERS.add(this);
+    }
+
+    public static OSLogger getFileLogger(String path) throws FileNotFoundException {
+        return getFileLogger(path, null);
+    }
+
+    public static OSLogger getFileLogger(String path, String format) throws FileNotFoundException {
+        return getFileLogger(new File(path), format);
+    }
+
+    public static OSLogger getFileLogger(File file, String format) throws FileNotFoundException {
+        return getFileLogger(new FileOutputStream(file), format);
+    }
+
+    public static OSLogger getFileLogger(FileOutputStream fos, String format) {
+        return new OSLogger(fos, format);
+    }
+
+    public static OSLogger getFileLogger(File file) throws FileNotFoundException {
+        return getFileLogger(file, null);
+    }
+
+    public static OSLogger getFileLogger(FileOutputStream fos) {
+        return getFileLogger(fos, null);
     }
 
     public static ILogger getSysoutLogger() {
@@ -87,5 +134,15 @@ public class OSLogger extends ALogger {
     @Override
     public void e(String msg, Throwable t) {
         ps.println(LogFormatter.formatLogMessage(LogLevel.ERROR, msg, t, this.format));
+    }
+
+    public boolean isClosed() {
+        return this.ps == null || this.closed;
+    }
+
+    public void close() {
+        this.ps.close();
+        this.closed = true;
+        OS_LOGGERS.remove(this);
     }
 }
