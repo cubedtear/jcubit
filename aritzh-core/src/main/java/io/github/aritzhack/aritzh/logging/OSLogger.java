7e4b1a8dadde19def6249ba6e15486a88fd00c26
Aritz Lopez <aritzhack@gmail.com>
2015-01-04 18:50:32 +0100

    + Added two utility methods tp get the OSLogger for System.out and System.err (lazily init) * Changed from using a PrintWriter to a PrintStream, since the former relies on the latter, and they former adds no advantage

--- aritzh-core/src/main/java/io/github/aritzhack/aritzh/logging/OSLogger.java
+++ aritzh-core/src/main/java/io/github/aritzhack/aritzh/logging/OSLogger.java
@@ -1,11 +1,12 @@
 package io.github.aritzhack.aritzh.logging;
 
 import io.github.aritzhack.aritzh.logging.core.ALogger;
+import io.github.aritzhack.aritzh.logging.core.ILogger;
 import io.github.aritzhack.aritzh.logging.core.LogLevel;
 import io.github.aritzhack.aritzh.util.Nullable;
 
 import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.PrintStream;
 
 /**
  * @author Aritz Lopez
@@ -14,7 +15,9 @@ public class OSLogger extends ALogger {
 
     private static final String DEFAULT_FORMAT = "%d - %L %m?t{\n%t}";
 
-    final PrintWriter pw;
+    private static ILogger OUT_LOGGER;
+    private static ILogger ERR_LOGGER;
+    final PrintStream ps;
     final String format;
 
     public OSLogger(OutputStream os) {
@@ -22,57 +25,67 @@ public class OSLogger extends ALogger {
     }
 
     public OSLogger(OutputStream os, @Nullable String format) {
-        this.pw = new PrintWriter(os, true);
+        this.ps = new PrintStream(os, true);
         this.format = format == null ? DEFAULT_FORMAT : format;
+    }
+
+    public static ILogger getSysoutLogger() {
+        if (OUT_LOGGER == null) OUT_LOGGER = new OSLogger(System.out);
+        return OUT_LOGGER;
+    }
+
+    public static ILogger getSyserrLogger() {
+        if (ERR_LOGGER == null) ERR_LOGGER = new OSLogger(System.err);
+        return ERR_LOGGER;
     }
 
     @Override
     public void t(String msg) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.TRACE, msg, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.TRACE, msg, this.format));
     }
 
     @Override
     public void t(String msg, Throwable t) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.TRACE, msg, t, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.TRACE, msg, t, this.format));
     }
 
     @Override
     public void d(String msg) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.DEBUG, msg, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.DEBUG, msg, this.format));
     }
 
     @Override
     public void d(String msg, Throwable t) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.DEBUG, msg, t, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.DEBUG, msg, t, this.format));
     }
 
     @Override
     public void i(String msg) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.INFO, msg, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.INFO, msg, this.format));
     }
 
     @Override
     public void i(String msg, Throwable t) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.INFO, msg, t, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.INFO, msg, t, this.format));
     }
 
     @Override
     public void w(String msg) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.WARN, msg, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.WARN, msg, this.format));
     }
 
     @Override
     public void w(String msg, Throwable t) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.WARN, msg, t, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.WARN, msg, t, this.format));
     }
 
     @Override
     public void e(String msg) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.ERROR, msg, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.ERROR, msg, this.format));
     }
 
     @Override
     public void e(String msg, Throwable t) {
-        pw.println(LogFormatter.formatLogMessage(LogLevel.ERROR, msg, t, this.format));
+        ps.println(LogFormatter.formatLogMessage(LogLevel.ERROR, msg, t, this.format));
     }
 }
