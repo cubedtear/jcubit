36f85a1434a26f3ccd9d7bdbf804c64357ec4cb4
Aritz Lopez <aritzhack@gmail.com>
2015-07-22 11:40:03 +0200

    + Implemented an UndirectedGraph class

---
+++ aritzh-core/src/main/java/io/github/aritzhack/aritzh/math/UndirectedGraph.java
@@ -0,0 +1,37 @@
+package io.github.aritzhack.aritzh.math;
+
+import io.github.aritzhack.aritzh.collections.HashMultiBiMap;
+
+import java.util.Set;
+
+/**
+ * @author Aritz Lopez
+ */
+public class UndirectedGraph<T> {
+
+	private final HashMultiBiMap<T, T> graph = HashMultiBiMap.create();
+
+	private UndirectedGraph() {
+
+	}
+
+	public static <T> UndirectedGraph<T> create() {
+		return new UndirectedGraph<>();
+	}
+
+	public void addEdge(T t1, T t2) {
+		this.graph.put(t1, t2);
+	}
+
+	public Set<T> adjacents(T t) {
+		return graph.get(t);
+	}
+
+	public int degree(T t) {
+		return adjacents(t).size();
+	}
+
+	public Set<T> getVertices() {
+		return graph.keys();
+	}
+}
