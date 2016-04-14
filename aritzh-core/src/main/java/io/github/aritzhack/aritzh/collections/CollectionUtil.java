81b1c0392cf004bff00f978562d0399f36fe2554
Aritz Lopez <aritzhack@gmail.com>
2016-04-14 18:09:59 +0200

    Revert "Revert "* Retrolambda didn't work, so manually reverted""
    This reverts commit 1a984a592e23de8e61c295d812806418985ac7a5.

--- aritzh-core/src/main/java/io/github/aritzhack/aritzh/collections/CollectionUtil.java
+++ aritzh-core/src/main/java/io/github/aritzhack/aritzh/collections/CollectionUtil.java
@@ -21,102 +21,96 @@ import com.google.common.base.Function;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
  * @author Aritz Lopez
  */
 public enum CollectionUtil {
-	;
+    ;
 
-	/**
-	 * Applies a function to all elements in the collection. Note: Uses a for-each loop.
-	 * The return value of the function is ignored.
-	 *
-	 * @param coll     The collection of elements to apply the function to.
-	 * @param function The function to apply.
-	 * @param <I>      Collection content type.
-	 * @deprecated Use {@link Collection#forEach(Consumer)} instead.
-	 */
-	@Deprecated
-	public static <I> void applyToAll(Collection<I> coll, Function<I, ?> function) {
-		coll.forEach(function::apply);
-	}
+    /**
+     * Applies a function to all elements in the collection. Note: Uses a for-each loop.
+     * The return value of the function is ignored.
+     *
+     * @param coll     The collection of elements to apply the function to.
+     * @param function The function to apply.
+     * @param <I>      Collection content type.
+     */
+    @Deprecated
+    public static <I> void applyToAll(Collection<I> coll, Function<I, ?> function) {
+        for (I t : coll) function.apply(t);
+    }
 
-	/**
-	 * Applies a function to the keys of the map. Note: Uses a for-each loop.
-	 *
-	 * @param map      The map with the keys the function will be applied to.
-	 * @param function The function to apply.
-	 * @param <K>      The type of the keys of the map.
-	 * @deprecated Use {@link Collection#forEach(Consumer) map.keySet().forEach()} instead.
-	 */
-	public static <K> void applyToKeys(Map<K, ?> map, Function<? super K, ?> function) {
-		map.keySet().forEach(function::apply);
-	}
+    /**
+     * Applies a function to the keys of the map. Note: Uses a for-each loop.
+     *
+     * @param map      The map with the keys the function will be applied to.
+     * @param function The function to apply.
+     * @param <K>      The type of the keys of the map.
+     */
+    public static <K> void applyToKeys(Map<K, ?> map, Function<? super K, ?> function) {
+        for (K key : map.keySet()) function.apply(key);
+    }
 
-	/**
-	 * Applies a function to the values of the map. Note: Uses a for-each loop.
-	 *
-	 * @param map      The map with the values the function will be applied to.
-	 * @param function The function to apply.
-	 * @param <V>      The type of the values of the map.
-	 * @deprecated Use {@link Collection#forEach(Consumer) map.values().forEach()} instead.
-	 */
-	public static <V> void applyToValues(Map<?, V> map, Function<? super V, ?> function) {
-		map.values().forEach(function::apply);
-	}
+    /**
+     * Applies a function to the values of the map. Note: Uses a for-each loop.
+     *
+     * @param map      The map with the values the function will be applied to.
+     * @param function The function to apply.
+     * @param <V>      The type of the values of the map.
+     */
+    public static <V> void applyToValues(Map<?, V> map, Function<? super V, ?> function) {
+        for (V v : map.values()) function.apply(v);
+    }
 
-	/**
-	 * Returns the sum of all integers from the iterable
-	 *
-	 * @param iter The iterable
-	 * @return the sum of all integers from the iterable
-	 * @deprecated Use {@link IntStream#sum()} instead
-	 */
-	@Deprecated
-	public static int integerSum(Iterable<Integer> iter) {
-		return StreamSupport.stream(iter.spliterator(), false).mapToInt(i -> i).sum();
-	}
+    /**
+     * Returns the sum of all integers from the iterable
+     *
+     * @param iter The iterable
+     * @return the sum of all integers from the iterable
+     */
+    public static int integerSum(Iterable<Integer> iter) {
+        int sum = 0;
+        for (Integer i : iter)
+            sum += i;
+        return sum;
+    }
 
-	/**
-	 * Returns the sum of all floats from the iterable
-	 *
-	 * @param iter The iterable
-	 * @return the sum of all floats from the iterable
-	 * @deprecated Use {@link DoubleStream#sum()} instead
-	 */
-	@Deprecated
-	public static float floatSum(Iterable<Float> iter) {
-		return (float) StreamSupport.stream(iter.spliterator(), false).mapToDouble(i -> i).sum();
-	}
+    /**
+     * Returns the sum of all floats from the iterable
+     *
+     * @param iter The iterable
+     * @return the sum of all floats from the iterable
+     */
+    public static float floatSum(Iterable<Float> iter) {
+        float sum = 0;
+        for (Float f : iter)
+            sum += f;
+        return sum;
+    }
 
-	/**
-	 * Returns the sum of all doubles from the iterable
-	 *
-	 * @param iter The iterable
-	 * @return the sum of all doubles from the iterable
-	 * @deprecated Use {@link DoubleStream#sum()} instead
-	 */
-	@Deprecated
-	public static double doubleSum(Iterable<Double> iter) {
-		return StreamSupport.stream(iter.spliterator(), false).mapToDouble(i -> i).sum();
-	}
+    /**
+     * Returns the sum of all doubles from the iterable
+     *
+     * @param iter The iterable
+     * @return the sum of all doubles from the iterable
+     */
+    public static double doubleSum(Iterable<Double> iter) {
+        double sum = 0;
+        for (Double d : iter)
+            sum += d;
+        return sum;
+    }
 
-	/**
-	 * Returns the last element of a list, or null if it is empty
-	 *
-	 * @param list The list
-	 * @param <T>  The type of the contents of the list
-	 * @return the last element of a list, or null if it is empty
-	 */
-	public static <T> T getLast(List<T> list) {
-		return list.isEmpty() ? null : list.get(list.size() - 1);
-	}
+    /**
+     * Returns the last element of a list, or null if it is empty
+     *
+     * @param list The list
+     * @param <T>  The type of the contents of the list
+     * @return the last element of a list, or null if it is empty
+     */
+    public static <T> T getLast(List<T> list) {
+        return list.isEmpty() ? null : list.get(list.size() - 1);
+    }
 
 }
