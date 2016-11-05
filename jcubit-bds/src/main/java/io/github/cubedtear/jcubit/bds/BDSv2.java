faa27a7237707ff4009903ca4ac683d423e65af2
Aritz Lopez <cubedtear@gmail.com>
2016-11-05 21:19:29 +0100

    + Added Javadoc to BDSv2

--- jcubit-bds/src/main/java/io/github/cubedtear/jcubit/bds/BDSv2.java
+++ jcubit-bds/src/main/java/io/github/cubedtear/jcubit/bds/BDSv2.java
@@ -12,10 +12,18 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
+ * Binary Data Storage version 2
+ * Used to serialize different data types into byte arrays or OutputStreams,
+ * write them somewhere (file, DB, network, etc.), and then read them back.
+ *
  * @author Aritz Lopez
  */
 public class BDSv2 {
 
+    /**
+     * The sequence of bytes that will always be at the beginning of a serialized BDSv2.
+     * Useful to detect a BDSv2-serialized file, when opened with a text editor.
+     */
     public final static byte[] SIGNATURE = ".BDSv2\r\n".getBytes(StandardCharsets.UTF_8);
 
     // region ... Collection of elements ...
@@ -44,10 +52,18 @@ public class BDSv2 {
 
     private int size = 0;
 
+    /**
+     * Creates an empty BDSv2.
+     */
     public BDSv2() {
 
     }
 
+    /**
+     * Calculates the bytes this BDSv2 will take once serialized.
+     *
+     * @return the size of this BDSv2 in bytes.
+     */
     public int getSize() {
         // Size of the signature: Integer (size) + data
         return SIGNATURE.length + 4 + getInternalSize();
@@ -74,48 +90,97 @@ public class BDSv2 {
     // region ... Adders ...
 
     // region ... Normal adders ...
+
+    /**
+     * Adds the given byte to this BDSv2 with the given name.
+     *
+     * @param name The name to give the byte.
+     * @param s    The byte to store.
+     */
     public void addByte(String name, byte s) {
         checkAndAddName(name);
         bytes.put(name, s);
         this.size += 1 + 1;
     }
 
+    /**
+     * Adds the given short to this BDSv2 with the given name.
+     *
+     * @param name The name to give the short.
+     * @param s    The short to store.
+     */
     public void addShort(String name, short s) {
         checkAndAddName(name);
         shorts.put(name, s);
         this.size += 2 + 1;
     }
 
+    /**
+     * Adds the given char to this BDSv2 with the given name.
+     *
+     * @param name The name to give the char.
+     * @param s    The char to store.
+     */
     public void addChar(String name, char s) {
         checkAndAddName(name);
         chars.put(name, s);
         this.size += 2 + 1;
     }
 
+    /**
+     * Adds the given int to this BDSv2 with the given name.
+     *
+     * @param name The name to give the int.
+     * @param s    The int to store.
+     */
     public void addInt(String name, int s) {
         checkAndAddName(name);
         ints.put(name, s);
         this.size += 4 + 1;
     }
 
+    /**
+     * Adds the given long to this BDSv2 with the given name.
+     *
+     * @param name The name to give the long.
+     * @param s    The long to store.
+     */
     public void addLong(String name, long s) {
         checkAndAddName(name);
         longs.put(name, s);
         this.size += 8 + 1;
     }
 
+    /**
+     * Adds the given float to this BDSv2 with the given name.
+     *
+     * @param name The name to give the float.
+     * @param s    The float to store.
+     */
     public void addFloat(String name, float s) {
         checkAndAddName(name);
         floats.put(name, s);
         this.size += 4 + 1;
     }
 
+    /**
+     * Adds the given double to this BDSv2 with the given name.
+     *
+     * @param name The name to give the double.
+     * @param s    The double to store.
+     */
     public void addDouble(String name, double s) {
         checkAndAddName(name);
         doubles.put(name, s);
         this.size += 8 + 1;
     }
 
+    /**
+     * Adds the given string to this BDSv2 with the given name.
+     *
+     * @param name The name to give the string.
+     * @param s    The string to store.
+     */
     public void addString(String name, String s) {
         try {
             checkAndAddName(name);
@@ -127,6 +192,12 @@ public class BDSv2 {
         }
     }
 
+    /**
+     * Adds the given BDSv2 as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the BDSv2.
+     * @param s    The BDSv2 to store.
+     */
     public void addBDS(String name, BDSv2 s) {
         checkAndAddName(name);
         bdss.put(name, s);
@@ -135,48 +206,97 @@ public class BDSv2 {
     // endregion
 
     // region ... Array adders ...
+
+    /**
+     * Adds the given byte array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the byte array.
+     * @param s    The byte array to store.
+     */
     public void addBytes(String name, byte[] s) {
         checkAndAddName(name);
         byteArrays.put(name, s);
         this.size += s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given short array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the short array.
+     * @param s    The short array to store.
+     */
     public void addShorts(String name, short[] s) {
         checkAndAddName(name);
         shortArrays.put(name, s);
         this.size += 2 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given char array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the char array.
+     * @param s    The char array to store.
+     */
     public void addChars(String name, char[] s) {
         checkAndAddName(name);
         charArrays.put(name, s);
         this.size += 2 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given int array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the int array.
+     * @param s    The int array to store.
+     */
     public void addInts(String name, int[] s) {
         checkAndAddName(name);
         intArrays.put(name, s);
         this.size += 4 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given long array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the long array.
+     * @param s    The long array to store.
+     */
     public void addLongs(String name, long[] s) {
         checkAndAddName(name);
         longArrays.put(name, s);
         this.size += 8 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given float array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the float array.
+     * @param s    The float array to store.
+     */
     public void addFloats(String name, float[] s) {
         checkAndAddName(name);
         floatArrays.put(name, s);
         this.size += 4 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given double array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the double array.
+     * @param s    The double array to store.
+     */
     public void addDoubles(String name, double[] s) {
         checkAndAddName(name);
         doubleArrays.put(name, s);
         this.size += 8 * s.length + 1 + 4;
     }
 
+    /**
+     * Adds the given String array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the String array.
+     * @param s    The String array to store.
+     */
     public void addStrings(String name, String[] s) {
         checkAndAddName(name);
         Set2<String, byte[]>[] array = new Set2[s.length];
@@ -194,6 +314,12 @@ public class BDSv2 {
         this.size += 1 + 4;
     }
 
+    /**
+     * Adds the given BDSv2 array as child to this BDSv2 with the given name.
+     *
+     * @param name The name to give the BDSv2 array.
+     * @param s    The BDSv2 array to store.
+     */
     public void addBDSs(String name, BDSv2[] s) {
         checkAndAddName(name);
         bdsArrays.put(name, s);
@@ -207,38 +333,92 @@ public class BDSv2 {
 
     // region ... Normal getters ...
 
+    /**
+     * Gets the byte with the given name from this BDSv2.
+     *
+     * @param name The name of the byte.
+     * @return the byte with the given name.
+     */
     public Byte getByte(String name) {
         return this.bytes.get(name);
     }
 
+    /**
+     * Gets the char with the given name from this BDSv2.
+     *
+     * @param name The name of the char.
+     * @return the char with the given name.
+     */
     public Character getChar(String name) {
         return this.chars.get(name);
     }
 
+    /**
+     * Gets the short with the given name from this BDSv2.
+     *
+     * @param name The name of the short.
+     * @return the short with the given name.
+     */
     public Short getShort(String name) {
         return this.shorts.get(name);
     }
 
+    /**
+     * Gets the int with the given name from this BDSv2.
+     *
+     * @param name The name of the int.
+     * @return the int with the given name.
+     */
     public Integer getInt(String name) {
         return this.ints.get(name);
     }
 
+    /**
+     * Gets the long with the given name from this BDSv2.
+     *
+     * @param name The name of the long.
+     * @return the long with the given name.
+     */
     public Long getLong(String name) {
         return this.longs.get(name);
     }
 
+    /**
+     * Gets the float with the given name from this BDSv2.
+     *
+     * @param name The name of the float.
+     * @return the float with the given name.
+     */
     public Float getFloat(String name) {
         return this.floats.get(name);
     }
 
+    /**
+     * Gets the double with the given name from this BDSv2.
+     *
+     * @param name The name of the double.
+     * @return the double with the given name.
+     */
     public Double getDouble(String name) {
         return this.doubles.get(name);
     }
 
+    /**
+     * Gets the String with the given name from this BDSv2.
+     *
+     * @param name The name of the String.
+     * @return the String with the given name.
+     */
     public String getString(String name) {
         return this.strings.get(name).getT();
     }
 
+    /**
+     * Gets the BDSv2 with the given name from this BDSv2.
+     *
+     * @param name The name of the BDSv2.
+     * @return the BDSv2 with the given name.
+     */
     public BDSv2 getBDS(String name) {
         return this.bdss.get(name);
     }
@@ -247,34 +427,82 @@ public class BDSv2 {
 
     // region ... Array getters ...
 
+    /**
+     * Gets the byte array with the given name from this BDSv2.
+     *
+     * @param name The name of the byte array.
+     * @return the byte array with the given name.
+     */
     public byte[] getBytes(String name) {
         return this.byteArrays.get(name);
     }
 
+    /**
+     * Gets the char array with the given name from this BDSv2.
+     *
+     * @param name The name of the char array.
+     * @return the char array with the given name.
+     */
     public char[] getChars(String name) {
         return this.charArrays.get(name);
     }
 
+    /**
+     * Gets the short array with the given name from this BDSv2.
+     *
+     * @param name The name of the short array.
+     * @return the short array with the given name.
+     */
     public short[] getShorts(String name) {
         return this.shortArrays.get(name);
     }
 
+    /**
+     * Gets the int array with the given name from this BDSv2.
+     *
+     * @param name The name of the int array.
+     * @return the int array with the given name.
+     */
     public int[] getInts(String name) {
         return this.intArrays.get(name);
     }
 
+    /**
+     * Gets the long array with the given name from this BDSv2.
+     *
+     * @param name The name of the long array.
+     * @return the long array with the given name.
+     */
     public long[] getLongs(String name) {
         return this.longArrays.get(name);
     }
 
+    /**
+     * Gets the float array with the given name from this BDSv2.
+     *
+     * @param name The name of the float array.
+     * @return the float array with the given name.
+     */
     public float[] getFloats(String name) {
         return this.floatArrays.get(name);
     }
 
+    /**
+     * Gets the double array with the given name from this BDSv2.
+     *
+     * @param name The name of the double array.
+     * @return the double array with the given name.
+     */
     public double[] getDoubles(String name) {
         return this.doubleArrays.get(name);
     }
 
+    /**
+     * Gets the String array with the given name from this BDSv2.
+     *
+     * @param name The name of the String array.
+     * @return the String array with the given name.
+     */
     public String[] getStrings(String name) {
         Set2<String, byte[]>[] strs = this.stringArrays.get(name);
         if (strs == null) return null;
@@ -285,6 +513,12 @@ public class BDSv2 {
         return strings;
     }
 
+    /**
+     * Gets the BDSv2 array with the given name from this BDSv2.
+     *
+     * @param name The name of the BDSv2 array.
+     * @return the BDSv2 array with the given name.
+     */
     public BDSv2[] getBDSs(String name) {
         return this.bdsArrays.get(name);
     }
@@ -295,6 +529,11 @@ public class BDSv2 {
 
     // region ... Writers ...
 
+    /**
+     * Writes this BDSv2 to a byte array.
+     *
+     * @return A byte array containing this BDSv2, serialized.
+     */
     public byte[] write() {
         byte[] data = new byte[this.getSize()];
         try {
@@ -305,6 +544,9 @@ public class BDSv2 {
         return data;
     }
 
+    /**
+     * Writes this BDSv2 to an OutputStream.
+     */
     public void writeToStream(OutputStream os) throws IOException {
         os.write(SIGNATURE);
         writeInternal(os);
@@ -480,10 +722,25 @@ public class BDSv2 {
 
     // region ... Parsers ...
 
+    /**
+     * Parses a BDSv2 from the beginning of the given byte array.
+     *
+     * @param data The array from which to parse.
+     * @return The parsed BDSv2.
+     * @throws SerializationException If there is an error deserializing.
+     */
     public static BDSv2 parse(byte[] data) throws SerializationException {
         return BDSv2.parse(data, 0);
     }
 
+    /**
+     * Parses a BDSv2 from the beginning of the given byte array.
+     *
+     * @param data   The array from which to parse.
+     * @param offset The index of the array from which to start parsing the BDSv2.
+     * @return The parsed BDSv2.
+     * @throws SerializationException If there is an error deserializing.
+     */
     public static BDSv2 parse(byte[] data, int offset) throws SerializationException {
         try {
             for (byte b : SIGNATURE) {
@@ -496,6 +753,13 @@ public class BDSv2 {
         }
     }
 
+    /**
+     * Parses a BDSv2 from an InputStream.
+     *
+     * @param is The InputStream to parse from.
+     * @return The parsed BDSv2.
+     * @throws SerializationException If there is an error deserializing.
+     */
     public static BDSv2 parseStream(InputStream is) throws IOException, SerializationException {
         for (byte b : SIGNATURE)
             if ((byte) is.read() != b) throw new SerializationException("BDSv2 signature not present, or incorrect!");
